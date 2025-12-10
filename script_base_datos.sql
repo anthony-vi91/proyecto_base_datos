06dcfca1e7c4a59a9547af823380ff1cd1841e39
Anthony-Vite <avite.es@tecnologicoargos.edu.ec>
2025-12-09 19:10:23 -0500

    Migrate schema from tienda to veterinaria

--- script_base_datos.sql
+++ script_base_datos.sql
@@ -1,90 +1,118 @@
-create schema tienda;
+create schema veterinaria
 
-CREATE TABLE tienda.Cliente (
-  id_cliente SERIAL PRIMARY KEY,
-	tipo_identificacion VARCHAR(10) NOT NULL CHECK (tipo_identificacion IN ('CEDULA', 'RUC', 'PASAPORTE')),
-  identificacion VARCHAR(13) NOT NULL,
-  nombre VARCHAR(100) NOT NULL,
-  apellido VARCHAR(100) NOT NULL,
-  direccion TEXT,
-  telefono VARCHAR(20),
-  email VARCHAR(255) UNIQUE NOT NULL,
-  activo BOOLEAN DEFAULT TRUE,
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  actualizado_en TIMESTAMP,
-	UNIQUE (tipo_identificacion, identificacion)
+--   TABLA CLIENTES
+CREATE TABLE veterinaria.clientes (
+    id_cliente serial PRIMARY KEY,
+    nombre VARCHAR(50)NOT NULL,
+    tipo_identificacion VARCHAR(10) NOT NULL CHECK (tipo_identificacion IN ('CEDULA', 'RUC', 'PASAPORTE')),
+    identificacion VARCHAR(20) UNIQUE NOT NULL,
+    telefono VARCHAR(20),
+    email VARCHAR(100) NOT NULL,
+    direccion VARCHAR(150),
+    activo BOOLEAN DEFAULT TRUE,
+    creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    actualizado_en TIMESTAMP,
+   UNIQUE (tipo_identificacion, identificacion)
 );
 
-CREATE TABLE tienda.Tipo_Producto (
-  id_tipo_producto SERIAL PRIMARY KEY,
-  nombre VARCHAR(100) UNIQUE NOT NULL,
-  descripcion TEXT,
-  activo BOOLEAN DEFAULT TRUE,
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  actualizado_en TIMESTAMP
+
+--   TABLA MASCOTAS
+CREATE TABLE veterinaria.mascotas (
+    id_mascota serial PRIMARY KEY,
+    id_cliente INT,
+    nombre VARCHAR(50) not null,
+    especie VARCHAR(30) not null,
+    raza VARCHAR(50) not null,
+    sexo VARCHAR(10) not null,
+    fecha_nacimiento DATE,
+    alergias TEXT,
+    FOREIGN KEY (id_cliente) REFERENCES veterinaria.clientes(id_cliente),
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_registro DATE DEFAULT CURRENT_DATE,
+    fecha_actualizado_en TIMESTAMP
 );
 
-CREATE TABLE tienda.Proveedor (
-  id_proveedor SERIAL PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  contacto VARCHAR(100),
-  telefono VARCHAR(20),
-  direccion TEXT,
-  email VARCHAR(255),
-  activo BOOLEAN DEFAULT TRUE,
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  actualizado_en TIMESTAMP
+--   TABLA CITAS
+CREATE TABLE citas (
+    id_cita serial PRIMARY KEY,
+    id_mascota INT not null,
+    fecha_cita DATETIME,
+    motivo TEXT not null,
+    diagnostico TEXT not null,
+    estado VARCHAR(20),
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_registro DATE DEFAULT CURRENT_DATE,
+    fecha_actualizado_en TIMESTAMP,
+    FOREIGN KEY (id_mascota) REFERENCES veterinaria.mascotas(id_mascota)
 );
 
-CREATE TABLE tienda.Producto (
-  id_producto SERIAL PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  descripcion TEXT,
-  precio DECIMAL(10, 2) NOT NULL CHECK (precio >= 0),
-  costo DECIMAL(10, 2) CHECK (costo >= 0),
-  id_tipo_producto INT NOT NULL,
-  id_proveedor INT NOT NULL,
-  activo BOOLEAN DEFAULT TRUE,
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  actualizado_en TIMESTAMP,
-  FOREIGN KEY (id_tipo_producto) REFERENCES tienda.Tipo_Producto(id_tipo_producto),
-  FOREIGN KEY (id_proveedor) REFERENCES tienda.Proveedor(id_proveedor)
+
+--   TABLA PROVEEDORES
+CREATE TABLE proveedores (
+    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    telefono VARCHAR(20),
+    email VARCHAR(100),
+    direccion VARCHAR(150),
+    ruc VARCHAR(20),
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_registro DATE DEFAULT CURRENT_DATE
+    fecha_actualizado_en TIMESTAMP
 );
 
-CREATE TABLE tienda.Inventario (
-  id_inventario SERIAL PRIMARY KEY,
-  id_producto INT UNIQUE NOT NULL,
-  cantidad INT NOT NULL CHECK (cantidad >= 0),
-	activo BOOLEAN DEFAULT TRUE,
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  actualizado_en TIMESTAMP,
-  FOREIGN KEY (id_producto) REFERENCES tienda.Producto(id_producto)
+
+--   TABLA PRODUCTOS
+CREATE TABLE productos (
+    id_producto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    descripcion TEXT,
+    categoria VARCHAR(50),
+    precio_venta DECIMAL(10,2),
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_registro DATE DEFAULT CURRENT_DATE
+    fecha_actualizado_en TIMESTAMP
 );
 
-CREATE TABLE tienda.Factura (
-  id_factura SERIAL PRIMARY KEY,
-  id_cliente INT NOT NULL,
-  fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  estado VARCHAR(20) DEFAULT 'PENDIENTE' CHECK (estado IN ('PENDIENTE', 'PAGADA', 'ANULADA')),
-  subtotal DECIMAL(10, 2) NOT NULL CHECK (subtotal >= 0),
-  impuesto DECIMAL(10, 2) DEFAULT 0 CHECK (impuesto >= 0),
-  total DECIMAL(10, 2) NOT NULL CHECK (total >= 0),
-  activo BOOLEAN DEFAULT TRUE,
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  actualizado_en TIMESTAMP,
-  FOREIGN KEY (id_cliente) REFERENCES tienda.Cliente(id_cliente)
+
+--   TABLA INVENTARIO
+CREATE TABLE inventario (
+    id_inventario INT AUTO_INCREMENT PRIMARY KEY,
+    id_producto INT,
+    id_proveedor INT,
+    stock_actual INT,
+    fecha_actualizacion DATE,
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_registro DATE DEFAULT CURRENT_DATE
+    fecha_actualizado_en TIMESTAMP,
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
 );
 
-CREATE TABLE tienda.Detalle_Factura (
-  id_detalle SERIAL PRIMARY KEY,
-  id_factura INT NOT NULL,
-  id_producto INT NOT NULL,
-  cantidad INT NOT NULL CHECK (cantidad > 0),
-  precio_unitario DECIMAL(10, 2) NOT NULL CHECK (precio_unitario >= 0),
-  subtotal_linea DECIMAL(10, 2) NOT NULL CHECK (subtotal_linea >= 0),
-	activo BOOLEAN DEFAULT TRUE,
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	actualizado_en TIMESTAMP,
-  FOREIGN KEY (id_factura) REFERENCES tienda.Factura(id_factura),
-  FOREIGN KEY (id_producto) REFERENCES tienda.Producto(id_producto)
+
+--   TABLA VENTAS
+CREATE TABLE ventas (
+    id_venta INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT,
+    fecha_venta DATE,
+    total DECIMAL(10,2),
+    metodo_pago VARCHAR(20),
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_registro DATE DEFAULT CURRENT_DATE
+    fecha_actualizado_en TIMESTAMP,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+
+--   TABLA DETALLE FACTURA
+CREATE TABLE detalle_factura (
+    id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+    id_venta INT,
+    id_producto INT,
+    cantidad INT,
+    subtotal DECIMAL(10,2),
+    activo BOOLEAN DEFAULT TRUE,
+    fecha_registro DATE DEFAULT CURRENT_DATE
+    fecha_actualizado_en TIMESTAMP,
+    FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
+    FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
